30951b66716b2b10b57dc54bc71102b6a167a5dc
Joe <jparkkennaby@gmail.com>
2020-08-01 15:56:21 +0100

    setup script.js

---
+++ src/scripts/index.js
@@ -0,0 +1,15 @@
+/**
+ * Big 0 Algorithms:
+ * The Big 0 is not measured by seconds, but the size of the input compared to the number of operations
+ * A things grow larger and larger, does it scale?
+ */
+
+import On from './0(n)';
+
+console.log('Big 0 Algorithms Started');
+
+
+
+
+console.log('Big 0 Algorithms Ended');
+
