2cd23c0729119b7e7bc0e954635396abcd48fb76
Joe <jparkkennaby@gmail.com>
2020-08-01 20:39:17 +0100

    add big O example 4 - demontrating Rule 3 - Different terms for inputs

---
+++ src/scripts/big-0-calculation-examples/04.js
@@ -0,0 +1,15 @@
+function compressItems(items, items2) {
+
+    items.forEach(function (items) {
+        console.log(items);
+    });
+
+    items2.forEach(function (items) {
+        console.log(items);
+    });
+
+}
+
+// BIG O(items + items2)
+
+// BIG O(n + n)
