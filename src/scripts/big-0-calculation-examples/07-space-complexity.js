83fb3602669cfc043513d0e5b91485cec6b4a0fe
Joe <jparkkennaby@gmail.com>
2020-08-15 16:58:16 +0100

    add 07-space-complexity.js Big O calculation example

---
+++ src/scripts/big-0-calculation-examples/07-space-complexity.js
@@ -0,0 +1,22 @@
+//#5 Space complexity O(1)
+function boooo(n) {
+    for (let i = 0; i < n; i++) {
+        console.log('booooo');
+    }
+}
+
+boooo(n)
+
+
+// -------
+
+// #6 Space complexity O(n)
+function arrayOfHiNTimes(n) {
+    var hiArray = [];
+    for (let i = 0; i < n; i++) {
+        hiArray[i] = 'hi';
+    }
+    return hiArray;
+}
+
+arrayOfHiNTimes(6)
